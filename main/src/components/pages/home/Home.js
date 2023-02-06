8aebe661bd578b8aa1e236ad86af3bb4849682af
nkpe <naomi.pearse@gmail.com>
2023-02-06 14:51:37 +0000

    responsive view for home page fixed - but width too short

--- main/src/components/pages/home/Home.js
+++ main/src/components/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Home.css';
+import './HomeResponsive.css';
 import { Link } from 'react-router-dom';
 
 import newIn from '../../../images/new_in.jpg';
@@ -7,34 +8,10 @@ import blog from '../../../images/blog.jpg';
 import sizeGuide from '../../../images/size_guide.jpg';
 import collection from '../../../images/collec_glitter.jpg';
 
-const HomeDesktop = () => {
+const HomeRightDesktop = () => {
     return (
-        <section id="homepage" className="page">
-            <Link to="/collections" id="lefthome" className="home-shape collec-glitter allproduct-btn">
-                <img src={collection} alt="Glitter Collection" />
-                <div id="lefthome-text" className="home-text">GLITTER NAILS</div>
-            </Link>
-
-            <div className="home-shape-divider"></div>
-
-            <Link to="/collections/new-season" id="midhome" className="home-shape">
-                <img src={newIn} alt="New Season" />
-                <div id="midhome-text" className="home-text">NEW SEASON</div>
-            </Link>
-
+        <>
             <div id="hs-div-2" className="home-shape-divider"></div>
-
-            <Link to="/custom" id="mob-bottomlefthome" className="home-shape blog-btn">
-                <div id="mob-bottomlefthome-text" className="home-text">Custom</div>
-                <img src={blog} alt="Custom" />
-            </Link>
-
-            <div id="mob-hs-div-2"></div>
-
-            <Link to="/unique-sizes" id="mob-bottomrighthome" className="home-shape uniquesize-btn">
-                <div id="mob-bottomrighthome-text" className="home-text uniquesize-btn">SIZE<br />GUIDE</div>
-                <img src={sizeGuide} alt="Size Guide" />
-            </Link>
 
             <div id="righthome" className="home-shape">
                 <Link to="/custom" id="toprighthome" className="blog-btn">
@@ -47,24 +24,12 @@ const HomeDesktop = () => {
                     <img src={sizeGuide} alt="Size Guide" />
                 </Link>
             </div>
-        </section>
+        </>
     );
 }
-const HomeMobile = () => {
+const HomeRightMobile = () => {
     return (
-        <section id="homepage" className="page">
-            <Link to="/collections" id="lefthome" className="home-shape collec-glitter allproduct-btn">
-                <img src={collection} alt="Glitter Collection" />
-                <div id="lefthome-text" className="home-text">GLITTER NAILS</div>
-            </Link>
-
-            <div className="home-shape-divider"></div>
-
-            <Link to="/collections/new-season" id="midhome" className="home-shape">
-                <img src={newIn} alt="New Season" />
-                <div id="midhome-text" className="home-text">NEW SEASON</div>
-            </Link>
-
+        <>
             <div id="hs-div-2" className="home-shape-divider"></div>
 
             <Link to="/custom" id="mob-bottomlefthome" className="home-shape blog-btn">
@@ -78,27 +43,46 @@ const HomeMobile = () => {
                 <div id="mob-bottomrighthome-text" className="home-text uniquesize-btn">SIZE<br />GUIDE</div>
                 <img src={sizeGuide} alt="Size Guide" />
             </Link>
-
-            <div id="righthome" className="home-shape">
-                <Link to="/custom" id="toprighthome" className="blog-btn">
-                    <div id="toprighthome-text" className="home-text">CUSTOM</div>
-                    <img src={blog} alt="Blog" />
-                </Link>
-
-                <Link to="/unique-sizes" id="bottomrighthome" className="uniquesize-btn">
-                    <div id="bottomrighthome-text" className="home-text">SIZE GUIDE</div>
-                    <img src={sizeGuide} alt="Size Guide" />
-                </Link>
-            </div>
-        </section>
+        </>
     );
+}
+
+const HomeRight = ( { mobView }) => {
+    if (mobView){
+        return <HomeRightMobile />
+    } else {
+        return <HomeRightDesktop />
+    }
 }
 
 const Home = () => {
+    let view = window.innerWidth;
+    let mobView;
+
+    if (view <= 450){
+        mobView = true;
+    } else {
+        mobView = false;
+    }
+
     return (
-        <>
-        {console.log("conditional")}
-        </>
+        <section id="homepage" className="page">
+        <Link to="/collections" id="lefthome" className="home-shape collec-glitter allproduct-btn">
+            <img src={collection} alt="Glitter Collection" />
+            <div id="lefthome-text" className="home-text">GLITTER NAILS</div>
+        </Link>
+
+        <div className="home-shape-divider"></div>
+
+        <Link to="/collections/new-season" id="midhome" className="home-shape">
+            <img src={newIn} alt="New Season" />
+            <div id="midhome-text" className="home-text">NEW SEASON</div>
+        </Link>
+        
+
+        {<HomeRight mobView={mobView} />}
+
+    </section>
         
     );
 }
