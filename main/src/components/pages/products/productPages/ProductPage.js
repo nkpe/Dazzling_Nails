c149c13a109a66eacaca1158cb34562dd1029e23
nkpe <naomi.pearse@gmail.com>
2023-02-06 12:49:07 +0000

    CSS Grid set up for Product Pages

--- main/src/components/pages/products/productPages/ProductPage.js
+++ main/src/components/pages/products/productPages/ProductPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Products from '../../../../data/products';
+
+import './ProductPage.css';
 
 // use state to handle product id?
 
@@ -8,20 +10,21 @@ const ProductPageMain = () => {
     const productId = location.pathname.slice(10);
     Products.forEach((item) => console.log(item.id))
     const product = Products.find((item) => item.id === Number(productId));
-    
 
     const imageKeyImport = Object.keys(product.image);
     const imageSrc = product.image[imageKeyImport];
     return (
-        <div>
-            <h1>{product.name}</h1>
-            <img src={imageSrc} alt={product.alt}/>
-            <p id="prod-descrip">{product.description}</p>
-            <Link to="/contact">Enquire Now</Link>
+        <div id="product-page">
+            <img id="prod-pg-img" src={imageSrc} alt={product.alt} />
+            <div id="prod-pg-content">
+                <h1>{product.name}</h1>
+                <p id="prod-descrip">{product.description}</p>
+                <Link to="/contact">Enquire Now</Link>
+            </div>
         </div>
-        
+
 
     )
 }
 
-export {ProductPageMain as default}
+export { ProductPageMain as default }
