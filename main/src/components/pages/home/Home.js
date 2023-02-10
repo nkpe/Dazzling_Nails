e5e0a6f6e988f000c271e8924a13df64334285f1
nkpe <naomi.pearse@gmail.com>
2023-02-10 11:15:03 +0000

    Revert "Revert "Skew CSS properties used""
    This reverts commit 7e297fccd9f3615da79b6057620520aa34172ea0.

--- main/src/components/pages/home/Home.js
+++ main/src/components/pages/home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Home.css';
-import './HomeResponsive.css';
+// import './HomeResponsive.css';
 import { Link } from 'react-router-dom';
 
 import newIn from '../../../images/new_in.jpg';
@@ -16,12 +16,12 @@ const HomeRightDesktop = () => {
             <div id="righthome" className="home-shape">
                 <Link to="/custom" id="toprighthome" className="blog-btn">
                     <div id="toprighthome-text" className="home-text">CUSTOM</div>
-                    <img src={blog} alt="Blog" />
+                    <img className="home-image" src={blog} alt="Blog" />
                 </Link>
 
                 <Link to="/unique-sizes" id="bottomrighthome" className="uniquesize-btn">
                     <div id="bottomrighthome-text" className="home-text">SIZE GUIDE</div>
-                    <img src={sizeGuide} alt="Size Guide" />
+                    <img className="home-image" src={sizeGuide} alt="Size Guide" />
                 </Link>
             </div>
         </>
@@ -30,18 +30,16 @@ const HomeRightDesktop = () => {
 const HomeRightMobile = () => {
     return (
         <>
-            <div id="hs-div-2" className="home-shape-divider"></div>
-
             <Link to="/custom" id="mob-bottomlefthome" className="home-shape blog-btn">
                 <div id="mob-bottomlefthome-text" className="home-text">Custom</div>
-                <img src={blog} alt="Custom" />
+                <img className="home-image" src={blog} alt="Custom" />
             </Link>
 
             <div id="mob-hs-div-2"></div>
 
             <Link to="/unique-sizes" id="mob-bottomrighthome" className="home-shape uniquesize-btn">
                 <div id="mob-bottomrighthome-text" className="home-text uniquesize-btn">SIZE<br />GUIDE</div>
-                <img src={sizeGuide} alt="Size Guide" />
+                <img className="home-image" src={sizeGuide} alt="Size Guide" />
             </Link>
         </>
     );
@@ -66,23 +64,22 @@ const Home = () => {
     }
 
     return (
-        <section id="homepage" className="page">
-        <Link to="/collections" id="lefthome" className="home-shape collec-glitter allproduct-btn">
-            <img src={collection} alt="Glitter Collection" />
-            <div id="lefthome-text" className="home-text">GLITTER NAILS</div>
-        </Link>
+        <section id="homepage">
+            <Link to="/collections" id="lefthome" className="home-shape collec-glitter allproduct-btn">
+                <img className="home-image" src={collection} alt="Glitter Collection" />
+                <div id="lefthome-text" className="home-text">GLITTER NAILS</div>
+            </Link>
 
-        <div className="home-shape-divider"></div>
+            <div className="home-shape-divider"></div>
 
-        <Link to="/collections/new-season" id="midhome" className="home-shape">
-            <img src={newIn} alt="New Season" />
-            <div id="midhome-text" className="home-text">NEW SEASON</div>
-        </Link>
-        
+            <Link to="/collections/new-season" id="midhome" className="home-shape">
+                <img className="home-image" src={newIn} alt="New Season" />
+                <div id="midhome-text" className="home-text">NEW SEASON</div>
+            </Link>
+            
 
-        {<HomeRight mobView={mobView} />}
-
-    </section>
+            {<HomeRight mobView={mobView} />}
+        </section>
         
     );
 }
